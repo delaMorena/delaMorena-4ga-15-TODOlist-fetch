73e6ce6ea36323bdffc2f9d199163330bb91840d
delaMorena <martadmdo@gmail.com>
2020-12-17 23:17:54 +0000

    request a la api y reponde con body. Pinto en html las tareas

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store";
 
 export default function(props) {
     const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        console.log("getListTodos: ", actions.getListTodos());
+        actions.getListTodos()
+        console.log("store.tasks: ", store.tasks) //por qué aparece vacío??
+        //no entiendo el orden de los console.log en la consola. Por qué se ejecuta en ese orden??
+    }, [])
+
     return (
         <div className="container-fluid">
             <h2>todos mis ToDos</h2>
             <div className="container">
                 <input
                     type="text"
-                    // onChange={handleChange}
-                    // onKeyPress={handleKeyPress}
                     placeholder="próxima tarea"
                     >
                 </input>
 
                 <h5>Mis tareas son:</h5>
                 <ul>
-                    {store.test.map((element, index) => {
+                    {store.tasks.map((element, index) => {
                         return (
                         <li key={index}>
                             <h5 className= "task">
--- src/store/flux.js
+++ src/store/flux.js
@@ -1,21 +1,46 @@
+// const fetch = requier("node-fetch");
+
 export default function({ getStore, getActions, setStore }) {
     return {
         store: {
-            loading: false,
             test: [
-                        { label: "Make the bed", done: false },
-                        { label: "Walk the dog", done: false },
-                        { label: "Do the replits", done: false }
-                    ]
+                { "label": "Make the bed", "done": false },
+                { "label": "Walk the dog", "done": false },
+                { "label": "Do the replits", "done": false }
+            ],
+            tasks: [],
+            // username: "delaMorena"
         },
         actions: {
-            setLoading(status) {
-                setStore({loading: status})
-            },
-            toggleLoader() {
+            getListTodos() {
                 const store = getStore()
-                setStore({loading: !store.loading})
+                const endpoint = "https://assets.breatheco.de/apis/fake/todos/user/delaMorena";
+                const config = { method: "GET" }
+
+                fetch(endpoint, config)
+                .then(response => {
+                    if(response.status >= 200 && response.status < 300){
+                        console.log(response.status);
+                        // console.log("response.json(): ", response.json())
+                        return response.json();
+                    }
+                    else{
+                        console.log(`There was an error ${response.status} with the request`);
+                    }   
+                })
+                .then((body) => {
+                    console.log("Este es el body del request", body);
+                    // body.map(task => task.label)
+                    // console.log(body.map(task => tasks.label).join(", "));
+                    setStore({ tasks: body })
+                    // console.log("tasks: ", tasks)
+                    setStore()
+                    console.log(store.tasks);
+                })
+                .catch(error => {
+                    console.error('Error:' ,error);
+                }); 
             }
         }
     }
-}
+};
