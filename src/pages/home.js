9cb2d145d547ac3ebfcfd7c06cb26377a2cdd007
delaMorena <martadmdo@gmail.com>
2020-12-18 15:02:57 +0000

    tutoria con sergio. Creada funcion updateTodos

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,32 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 
 import { Context } from "../store";
 
 export default function(props) {
     const { store, actions } = useContext(Context)
+    const [task, setTask] = useState();
+    
+    const handleChange = event => {
+        setTask(event.target.value);
+    };
+
+    const handleKeyPress = event => {
+		if (event.key === "Enter" && task != "") {
+            //1. llamamos a la funcion de update con el paramatro a incluir
+            actions.updateListTodos(task);
+            //necesito convertir  task en objeto con label y done. ahora es un string.
+            // setStore({ todos: task })
+            setTask ("");
+		}
+    };
 
     useEffect(() => {
-        console.log("getListTodos: ", actions.getListTodos());
+        console.log("getListTodos");
         actions.getListTodos()
-        console.log("store.tasks: ", store.tasks) //por qué aparece vacío??
-        //no entiendo el orden de los console.log en la consola. Por qué se ejecuta en ese orden??
+        console.log("store.username: ", store.username);
+        // console.log("store.tasks: ", store.tasks) 
     }, [])
+    
 
     return (
         <div className="container-fluid">
@@ -19,13 +34,16 @@ export default function(props) {
             <div className="container">
                 <input
                     type="text"
-                    placeholder="próxima tarea"
+                    placeholder = "próxima tarea"
+                    onChange = {handleChange}
+                    onKeyPress = {handleKeyPress}
+                    value = {task}
                     >
                 </input>
 
-                <h5>Mis tareas son:</h5>
+                <h5>Mis tareas son: {task}</h5>
                 <ul>
-                    {store.tasks.map((element, index) => {
+                    {store.todos.map((element, index) => {
                         return (
                         <li key={index}>
                             <h5 className= "task">
--- src/store/flux.js
+++ src/store/flux.js
@@ -1,26 +1,20 @@
-// const fetch = requier("node-fetch");
+
 
 export default function({ getStore, getActions, setStore }) {
     return {
         store: {
-            test: [
-                { "label": "Make the bed", "done": false },
-                { "label": "Walk the dog", "done": false },
-                { "label": "Do the replits", "done": false }
-            ],
-            tasks: [],
-            // username: "delaMorena"
+            todos: [],
+            username: "delaMorena"
         },
         actions: {
             getListTodos() {
                 const store = getStore()
-                const endpoint = "https://assets.breatheco.de/apis/fake/todos/user/delaMorena";
+                const endpoint = "https://assets.breatheco.de/apis/fake/todos/user/".concat(store.username);
                 const config = { method: "GET" }
 
                 fetch(endpoint, config)
                 .then(response => {
                     if(response.status >= 200 && response.status < 300){
-                        console.log(response.status);
                         // console.log("response.json(): ", response.json())
                         return response.json();
                     }
@@ -31,15 +25,41 @@ export default function({ getStore, getActions, setStore }) {
                 .then((body) => {
                     console.log("Este es el body del request", body);
                     // body.map(task => task.label)
-                    // console.log(body.map(task => tasks.label).join(", "));
-                    setStore({ tasks: body })
-                    // console.log("tasks: ", tasks)
-                    setStore()
-                    console.log(store.tasks);
+                    // console.log(body.map(task => todos.label).join(", "));
+                    setStore({ todos: body })
+                    // console.log("todos: ", todos)
+                    console.log("store.todos: ", store.todos);
                 })
                 .catch(error => {
                     console.error('Error:' ,error);
                 }); 
+            },
+            updateListTodos(changes){
+                console.log( "changes: ", changes)
+                const store = getStore()
+                const endpoint = "https://assets.breatheco.de/apis/fake/todos/user/" + store.username;
+                const config = { 
+                    method: "PUT", 
+                    body: JSON.stringify("Necesito meter aquí el objeto changes"),
+                    headers: {
+                        "Content-Type": "application/json" }
+                }
+            // fetch()
+                fetch(endpoint, config)
+                    .then(resp => {
+                        console.log(resp.ok); // will be true if the response is successfull
+                        console.log(resp.status); // the status code = 200 or code = 400 etc.
+                        console.log(resp.text()); // will try return the exact result as string
+                        return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+                    })
+                    .then(data => {
+                        //here is were your code should start after the fetch finishes
+                        console.log(data); //this will print on the console the exact object received from the server
+                    })
+                    .catch(error => {
+                        //error handling
+                        console.log(error);
+                    });
             }
         }
     }
